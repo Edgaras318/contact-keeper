8e584bb5920fbff73ca231bfba0507c218c7ab18
Spiridonovas,Edgaras E <e.spiridonovas@student.fontys.nl>
2021-02-16 15:17:19 +0100

    User Registration, Load User & Set Token

--- routes/users.js
+++ routes/users.js
@@ -22,6 +22,7 @@ router.post(
   ],
   async (req, res) => {
     const errors = validationResult(req, res);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
