b96d73544d8db7b20e50747d207afbae80ac3da6
Spiridonovas,Edgaras E <e.spiridonovas@student.fontys.nl>
2021-02-16 20:33:27 +0100

    Switch animation lib, private routes, spinner, CRUD contacts

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,32 +1,53 @@
-import React, { Fragment, useContext } from "react";
-import { CSSTransition, TransitionGroup } from "react-transition-group";
+import React, { Fragment, useContext, useEffect } from "react";
 import ContactItem from "./ContactItem";
+import Spinner from "../layout/Spinner";
 import ContactContext from "../../context/contact/contactContext";
+import { motion } from "framer-motion";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
 
-  const { contacts, filtered } = contactContext;
+  const { contacts, filtered, getContacts, loading } = contactContext;
 
-  if (contacts.length === 0) {
+  useEffect(() => {
+    getContacts();
+  }, []);
+
+  if (contacts !== null && contacts.length === 0 && !loading) {
     return <h4>Please add a contact</h4>;
   }
 
   return (
     <Fragment>
-      <TransitionGroup>
-        {filtered !== null
-          ? filtered.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames='item'>
-                <ContactItem contact={contact} />
-              </CSSTransition>
-            ))
-          : contacts.map(contact => (
-              <CSSTransition key={contact.id} timeout={500} classNames='item'>
-                <ContactItem contact={contact} />
-              </CSSTransition>
-            ))}
-      </TransitionGroup>
+      {contacts !== null && !loading ? (
+        filtered !== null ? (
+          filtered.map(contact => (
+            <motion.div
+              key={contact._id}
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.2 }}
+            >
+              <ContactItem contact={contact} />
+            </motion.div>
+          ))
+        ) : (
+          contacts.map(contact => (
+            <motion.div
+              key={contact._id}
+              layout
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.2 }}
+            >
+              <ContactItem contact={contact} />
+            </motion.div>
+          ))
+        )
+      ) : (
+        <Spinner />
+      )}
     </Fragment>
   );
 };
